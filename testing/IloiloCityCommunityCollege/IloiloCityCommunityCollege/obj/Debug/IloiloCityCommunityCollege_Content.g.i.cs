5d938a470682fcf3179c06c9818a9585dc393d5d
lucifer <lucifer@gwapo.com>
2016-09-17 22:10:11 +0800

    ang kandado

--- testing/IloiloCityCommunityCollege/IloiloCityCommunityCollege/obj/Debug/IloiloCityCommunityCollege_Content.g.i.cs
+++ testing/IloiloCityCommunityCollege/IloiloCityCommunityCollege/obj/Debug/IloiloCityCommunityCollege_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/untitled-1.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/lock.png")]
 
 
